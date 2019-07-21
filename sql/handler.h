86a5c6fcfe7347622337cec7ae963f7c387f1296
Aleksey Midenkov <midenok@gmail.com>
2019-07-21 23:30:49 +0300

    MDEV-17554 Auto-create new partition for system versioned tables with history partitioned by INTERVAL/LIMIT
    == Syntax change ==
    
    Keyword AUTO enables partition auto-creation.
    
    create table t1 (x int) with system versioning
    partition by system_time interval 1 hour auto;
    
    create table t1 (x int) with system versioning
    partition by system_time interval 1 month
    starts '2021-01-01 00:00:00' auto partitions 12;
    
    create table t1 (x int) with system versioning
    partition by system_time limit 1000 auto;
    
    Or with explicit partitions:
    
    create table t1 (x int) with system versioning
    partition by system_time interval 1 hour auto
    (partition p0 history, partition pn current);
    
    == Description ==
    
    Before executing history-generating DML command add N history
    partitions, so that N would be sufficient for potentially generated
    history. N > 1 may be required when history is rotated by INTERVAL and
    timestamp was jumped to future further than interval value.
    
    If last history partition exceeds LIMIT records before DML command
    then new history partition is created and the history filling is
    switched to the new partition. Thus LIMIT does not carry strict
    limitation and the history partition size must be planned as LIMIT
    records plus average number of history one DML generates.
    
    Auto-creation is implemented by synchronous
    fast_alter_partition_table() call from the thread of the executed DML
    command before the command itself (by the fallback and retry mechanism
    similar to Discovery feature, see Open_table_context).
    
    The name for newly added partitions are generated like default
    partition names with extension of MDEV-22155 (which avoids name
    clashes by extending assignment counter to next free-enough gap).
    
    These DML commands trigger auto-creation:
    
    * DELETE (including multi-delete, excluding DELETE HISTORY)
    * UPDATE (including multi-update)
    * REPLACE (including REPLACE .. SELECT)
    * INSERT .. ON DUPLICATE KEY UPDATE
    * LOAD DATA .. REPLACE
    
    MDEV-23642 Locking timeout caused by auto-creation affects original DML
    
    The reasons for this are:
    
    - Do not disrupt main business process (the history is auxiliary
    service);
    
    - Consequences are non-fatal (history is not lost, but comes into wrong
    partition; fixed by partitioning rebuild);
    
    - There is more freedom for application to fail in this case or
    not: it may read warning info and find corresponding error number.
    
    - While non-failing command is easy to handle by an application and
    fail it, the opposite is hard to handle: there is no automatic
    actions to fix failed command and retry, DBA intervention is
    required and until then application is non-functioning.
    
    MDEV-23639 Auto-create does not work under LOCK TABLES or inside triggers
    
    Don't do tdc_remove_table() for OT_ADD_HISTORY_PARTITION because it is
    not possible in locked tables mode.
    
    LTM_LOCK_TABLES mode (and LTM_PRELOCKED_UNDER_LOCK_TABLES) works out
    of the box as fast_alter_partition_table() can reopen tables via
    locked_tables_list.
    
    In LTM_PRELOCKED we reopen and relock table manually.
    
    More fixes
    
    * some_table_marked_for_reopen flag fix
    
    some_table_marked_for_reopen affets only reopen of
    m_locked_tables. I.e. Locked_tables_list::reopen_tables() reopens only
    tables from m_locked_tables.
    
    * Unused can_recover_from_failed_open() condition
    
    Is recover_from_failed_open() can be really used after
    open_and_process_routine()?

--- sql/handler.h
+++ sql/handler.h
@@ -832,6 +832,8 @@ typedef bool Log_func(THD*, TABLE*, bool, const uchar*, const uchar*);
 #define ALTER_PARTITION_TABLE_REORG (1ULL << 12)
 #define ALTER_PARTITION_CONVERT_IN  (1ULL << 13)
 #define ALTER_PARTITION_CONVERT_OUT (1ULL << 14)
+// Set for vers_add_auto_hist_parts() operation
+#define ALTER_PARTITION_AUTO_HIST   (1ULL << 15)
 
 /*
   This is master database for most of system tables. However there
@@ -2148,7 +2150,6 @@ struct Vers_parse_info: public Table_period_info
 
   Table_period_info::start_end_t as_row;
 
-protected:
   friend struct Table_scope_and_contents_source_st;
   void set_start(const LEX_CSTRING field_name)
   {
@@ -2160,6 +2161,8 @@ protected:
     as_row.end= field_name;
     period.end= field_name;
   }
+
+protected:
   bool is_start(const char *name) const;
   bool is_end(const char *name) const;
   bool is_start(const Create_field &f) const;
@@ -4257,6 +4260,8 @@ public:
   */
   virtual uint lock_count(void) const { return 1; }
   /**
+    Get the lock(s) for the table and perform conversion of locks if needed.
+
     Is not invoked for non-transactional temporary tables.
 
     @note store_lock() can return more than one lock if the table is MERGE
