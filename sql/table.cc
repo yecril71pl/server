61f2f6da16f231c91d95e6f923267aa5c236c40d
Aleksey Midenkov <midenok@gmail.com>
2021-03-01 17:38:11 +0300

    MDEV-16546 Replication of history modification
    4. Replication of system_versioning_insert_history via option_bits in
    OPTIONS_WRITTEN_TO_BIN_LOG.

--- sql/table.cc
+++ sql/table.cc
@@ -4118,7 +4118,7 @@ enum open_frm_error open_table_from_share(THD *thd, TABLE_SHARE *share,
   {
     if (!((*field_ptr)= share->field[i]->clone(&outparam->mem_root, outparam)))
       goto err;
-    if (thd->variables.force_fields_visible &&
+    if ((thd->variables.option_bits & OPTION_FORCE_FIELDS_VISIBLE) &&
         (*field_ptr)->invisible <= INVISIBLE_SYSTEM)
       (*field_ptr)->invisible= VISIBLE;
   }
