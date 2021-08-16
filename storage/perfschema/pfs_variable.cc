d7c366001444b7e820516daec6a746e3bba2a5be
Nikita Malyavin <nikitamalyavin@gmail.com>
2021-08-16 19:28:32 +0300

    do not lock LOCK_thd_data

--- storage/perfschema/pfs_variable.cc
+++ storage/perfschema/pfs_variable.cc
@@ -248,9 +248,6 @@ int PFS_system_variable_cache::do_materialize_all(THD *unsafe_thd)
     DEBUG_SYNC(m_current_thd, "materialize_session_variable_array_THD_locked");
     ret= make_call(&PFS_system_variable_cache::refresh_vars, 1);
 
-    /* Release lock taken in get_THD(). */
-    mysql_mutex_unlock(&m_safe_thd->LOCK_thd_data);
-
     m_materialized= true;
   }
   mysql_mutex_unlock(&LOCK_plugin_delete);
@@ -371,7 +368,6 @@ int PFS_system_variable_cache::make_call(Request_func func, uint param)
   {
     auto apc_call= new PFS_system_variable_cache_apc(this, func, param);
     bool timed_out;
-    mysql_mutex_lock(&m_safe_thd->LOCK_thd_kill);
     ret= m_safe_thd->apc_target.make_apc_call(requestor_thd, apc_call, 0,
                                               &timed_out);
   }
@@ -406,9 +402,6 @@ int PFS_system_variable_cache::do_materialize_session(PFS_thread *pfs_thread)
   {
     // Refresh all system vars
     ret= make_call(&PFS_system_variable_cache::refresh_vars, 0);
-
-    /* Release lock taken in get_THD(). */
-    mysql_mutex_unlock(&m_safe_thd->LOCK_thd_data);
 
     m_materialized= true;
   }
@@ -483,9 +476,6 @@ int PFS_system_variable_cache::do_materialize_session(THD *unsafe_thd)
   {
     // Refresh scope system vars
     ret= make_call(&PFS_system_variable_cache::refresh_vars, 0);
-
-    /* Release lock taken in get_THD(). */
-    mysql_mutex_unlock(&m_safe_thd->LOCK_thd_data);
 
     m_materialized= true;
   }
